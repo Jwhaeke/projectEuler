339afdd080955841966add3be3849d10d155d18f
jwhaeke <jwhaeke@live.nl>
2019-01-02 14:31:24 +0100

    finished problem 1 using an if-else loop

--- problem1.php
+++ problem1.php
@@ -7,7 +7,7 @@ Find the sum of all the multiples of 3 or 5 below 1000. -->
 
 $sum = 0;
 
-for ($i=0; $i <= 1000; $i++) { 
+for ($i=0; $i < 1000; $i++) { 
         if (($i % 3) == 0) {
         $sum += $i;
         echo $sum."<br>";
